fc4b56786eda0be97f349d01ddd31f4043398c14
unknown <njohnson@SWF20236.ad.swfwmd.net>
2015-06-17 11:59:59 -0400

    first push

---
+++ linearInterpImput.R
@@ -0,0 +1,14 @@
+## Time Series fill script for MFL time series
+require(tcltk) # open prompt for writing .csv file with filled data
+require(zoo) # time series fill functions
+
+stationData <- read.csv(file.choose(), stringsAsFactors = FALSE)
+zooStationData <- zoo(stationData[2:length(stationData[1,])],as.Date(stationData$date, format = "%m/%d/%Y"))
+filledZoo = na.approx(zooStationData, maxgap = 90, na.rm = FALSE) # fill in data with linear interpolation
+filledZoo = na.locf(filledZoo, maxgap = 10, fromLast = TRUE) # fill in data with nearest real value from back to forward to fill values at beggining
+filledZoo = na.locf(filledZoo, maxgap = 10, fromLast = FALSE) # fill in data with nearest real value forward to fill in end values
+filledDF = data.frame(filledZoo) # convert back to dataframe from zoo
+filledDF = data.frame(data.frame(row.names(filledDF)), filledDF) # add date column to filledDF
+colnames(filledDF)[1] = c("date")
+filledDF = filledDF[order(filledDF$date),] # Sort df.filled by date
+write.csv(filledDF, file = paste0(tclvalue(tcl("tk_getSaveFile")),".csv"), row.names = FALSE)
