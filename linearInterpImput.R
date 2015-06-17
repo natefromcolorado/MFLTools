6fd2a188065556e96c723c5cd922e4de577944a2
unknown <njohnson@SWF20236.ad.swfwmd.net>
2015-06-17 12:01:17 -0400

    added some comments

--- linearInterpImput.R
+++ linearInterpImput.R
@@ -3,7 +3,7 @@ require(tcltk) # open prompt for writing .csv file with filled data
 require(zoo) # time series fill functions
 
 stationData <- read.csv(file.choose(), stringsAsFactors = FALSE)
-zooStationData <- zoo(stationData[2:length(stationData[1,])],as.Date(stationData$date, format = "%m/%d/%Y"))
+zooStationData <- zoo(stationData[2:length(stationData[1,])],as.Date(stationData$date, format = "%m/%d/%Y")) # must create zoo obect for filling algorith
 filledZoo = na.approx(zooStationData, maxgap = 90, na.rm = FALSE) # fill in data with linear interpolation
 filledZoo = na.locf(filledZoo, maxgap = 10, fromLast = TRUE) # fill in data with nearest real value from back to forward to fill values at beggining
 filledZoo = na.locf(filledZoo, maxgap = 10, fromLast = FALSE) # fill in data with nearest real value forward to fill in end values
